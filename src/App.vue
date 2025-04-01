11d5e6964a4377e92ffc5db0ad4e0cc84afa3159
jiaopengzi <jiaopengzi@outlook.com>
2025-04-01 19:09:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { basicSetup, EditorView } from 'codemirror'
+import { vim, getCM } from '@replit/codemirror-vim'
+import { showPanel, type Panel } from '@codemirror/view'
+
+import { onMounted, useTemplateRef } from 'vue'
+
+defineOptions({ name: 'EditorCodemirror' })
+
+const codemirrorRef = useTemplateRef<HTMLElement | null>('codemirrorRef')
+
+function getVimMode(view: EditorView): string {
+  const cm = getCM(view)
+  if (cm && cm.state.vim && cm.state.vim.mode) {
+    return `--${cm.state.vim.mode}--`
+  }
+  return ''
+}
+
+function bottomPanel(view: EditorView): Panel {
+  const dom: HTMLElement | null = document.createElement('div')
+  dom.textContent = getVimMode(view)
+
+  const oldVimMode = getVimMode(view)
+
+  console.log('old:', oldVimMode)
+
+  return {
+    top: false,
+    dom,
+    update(update) {
+      const newVimMode = getVimMode(update.view)
+
+      // When pressing ESC to exit insert mode into normal mode, it still prints "new: --insert--"
+      // Logically it should print "new: --normal--" after ESC in insert mode
+      // Is this a bug?
+      // Note: Pressing ESC in non-insert modes correctly prints "new: --normal--"
+
+      console.log('new:', newVimMode)
+      dom.textContent = getVimMode(update.view)
+    },
+  }
+}
+
+let cmView: EditorView = null!
+
+const initializeCodeMirror = () => {
+  cmView = new EditorView({
+    doc: '',
+    extensions: [vim(), basicSetup, showPanel.of(bottomPanel)],
+    parent: codemirrorRef.value!,
+  })
+}
+
+onMounted(() => {
+  initializeCodeMirror()
+})
+</script>
+
+<template>
+  <div ref="codemirrorRef" id="my-codemirror"></div>
+</template>
+
+<style>
+#my-codemirror {
+  height: 600px;
+  width: 800px;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.cm-editor {
+  height: 100%;
+  width: 100%;
+}
+</style>
